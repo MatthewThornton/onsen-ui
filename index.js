4444bf314c8a8fc5e75f0d60f8e0b23f4734fb2f
Matthew Thornton <matthewthornton.usa@gmail.com>
2019-01-23 14:20:36 -0800

    add onsen and stylesheets

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import * as Ons from "react-onsenui"; 
 
 // main routes
 import AppRoutes from "./routes";
 import store from "./store";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <AppRoutes />
-    </Provider>,
+    <Ons.Page>
+        <Provider store={store}>
+            <AppRoutes />
+        </Provider>
+    </Ons.Page>,
     document.getElementById("app")
 )
